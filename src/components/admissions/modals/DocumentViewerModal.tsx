2e4ceefca269d5064fda528dd297589d88bfee63
Ahmed Mostafa <lofylofy56@gmail.com>
2026-02-18 20:57:25 +0200

    ...

--- src/components/admissions/modals/DocumentViewerModal.tsx
+++ src/components/admissions/modals/DocumentViewerModal.tsx
@@ -29,7 +29,7 @@ export default function DocumentViewerModal({
 
   return (
     <div
-      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4"
+      className="fixed inset-0 bg-black/50 bg-opacity-50 flex items-center justify-center z-50 p-4"
       onClick={onClose}
     >
       <div
